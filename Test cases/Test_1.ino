1f6fca6d234896b9a05ebafe38be59b3467512b6
Prathamesh Mawale <prathameshmawale@gmail.com>
2023-04-20 08:40:40 +0530

    added minor changes to code started with calibration code moved notes to new folder created test cases

---
+++ Test cases/Test_1.ino
@@ -0,0 +1,1 @@
+// test for rotating the robot and testing the motor
--- calibration.ino
+++ calibration.ino
@@ -5,17 +5,15 @@ const int right = 4;
 const int middle = 2;
 const int mid_left = 1;
 const int mid_right = 3;
+const int num_sens = 5;
+int thresh[num_sens];
 
 const int green = 5;
 const int blue = 3;
 const int red = 6;
 
-int lsp = 100;    // speed left
-int rsp = 100;    // speed right
-int lfspeed = 80; // what is speed
-int turnspeed;
-
-void setup(){
+void setup()
+{
     Serial.begin(9600);
     pinMode(startButton, INPUT_PULLUP);
     pinMode(7, OUTPUT);
@@ -24,9 +22,52 @@ void setup(){
     pinMode(green, OUTPUT); // green
     pinMode(red, OUTPUT);   // red
     //  lfspeed = 50000 / analogRead(7); //arbitrary conversion to convert analogRead to speed. Need to check if this works for all voltage levels
-    turnspeed = lfspeed * 0.6; // idk what is this
+    // turnspeed = lfspeed * 0.6; // idk what is this
 }
 
-void loop(){
+void loop()
+{
     // robot gol gol ghumta hai and takes values fast like helll
+    while (digitalRead(startButton) == 0) // waiting for the start button
+    {                                     // Do nothing while waiting for button press
+    }
+    calibration();
+    threshold();
+}
+
+void calibration()
+{
+    
+    delay(1000);
+    for (int i = 0; i < 3000; i++)
+    {
+        rotate();
+        for (int i; i < num_sens; i++)
+        {
+            if analogRead(i) < minValue[i]
+                Pass
+            if analogRead(i) > maxValue[i]
+                Pass
+        }
+    }
+}
+
+void threshold(){
+
+}
+// roatate the robot on place
+void rotate()
+{
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    analogWrite(9, 100);
+    analogWrite(10, 100);
+}
+
+void botstop() // stops the bot at the end
+{
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    analogWrite(9, 0);
+    analogWrite(10, 0);
 }
