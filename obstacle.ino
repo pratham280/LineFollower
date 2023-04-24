c8fc6dcc1c4774ee5199facca60235b0877d3ad3
Prathamesh Mawale <prathameshmawale@gmail.com>
2023-04-24 23:53:34 +0530

    testing features

--- obstacle.ino
+++ obstacle.ino
@@ -1,114 +1,218 @@
-const int startButton = 11;
 
-const int left = A0;
-const int ext_left = A5;
-const int mid_left = A1;
-const int middle = A2;
-const int mid_right = A3;
-const int ext_right = A6;
-const int right = A4;
-const int PingPin = 7;
-const int 
-// add ultrasonic logic and also the 
-const int green_led = 5;
-const int blue_led = 3;
-const int red_led = 6;
+#define enA 10 // Enable1 L298 Pin enA
+#define in1 9  // Motor1  L298 Pin in1
+#define in2 8  // Motor1  L298 Pin in1
+#define in3 7  // Motor2  L298 Pin in1
+#define in4 6  // Motor2  L298 Pin in1
+#define enB 5  // Enable2 L298 Pin enB
 
-bool l = 0;    // left
-bool r = 0;    // right
-bool s = 0;    // straight
-bool u = 0;    // back
-int e = 0;     // end of the maze
-int paths = 0; // over all path
+#define L_S A0 // ir sensor Left
+#define R_S A1 // ir sensor Right
 
-bool endFound = 0; // end found as it is
+#define echo A2    // Echo pin
+#define trigger A3 // Trigger pin
 
-int blackValue = 900; // analog value of black
-int whiteValue = 100; // analog value of white
-int threshold = 660;  // threshhold value invert this value to change to differnt contrast
-// int threshold = (blackValue + whiteValue) * 0.5;
+#define servo A5
 
-int FT = 250; // this is cycles based on how fast is the robot (cycles are inversely poroportional to speed)
-
-int P, D, I, previousError, PIDvalue, error; // ititate values pdi and error previous error and pid values calculated
-int lsp = 100;                               // speed left
-int rsp = 100;                               // speed right
-int lfspeed = 80;                            // what is speed
-int turnspeed;                               // turn speed will further change
-// gain values will alter later
-float Kp = 0.04;
-float Kd = 0.05;
-float Ki = 0.01;
-
-String str; // path as a string
-
-/*
-  Ping))) Sensor
-
-  This sketch reads a PING))) ultrasonic rangefinder and returns the distance
-  to the closest object in range. To do this, it sends a pulse to the sensor to
-  initiate a reading, then listens for a pulse to return. The length of the
-  returning pulse is proportional to the distance of the object from the sensor.
-
-  The circuit:
-    - +V connection of the PING))) attached to +5V
-    - GND connection of the PING))) attached to ground
-    - SIG connection of the PING))) attached to digital pin 7
-
-  created 3 Nov 2008
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Ping
-*/
-
-long duration, inches, cm;
+int Set = 15;
+int distance_L, distance_F, distance_R;
 
 void setup()
-{
-    Serial.begin(9600);
+{ // put your setup code here, to run once
+
+  Serial.begin(9600); // start serial communication at 9600bps
+
+  pinMode(R_S, INPUT); // declare if sensor as input
+  pinMode(L_S, INPUT); // declare ir sensor as input
+
+  pinMode(echo, INPUT);     // declare ultrasonic sensor Echo pin as input
+  pinMode(trigger, OUTPUT); // declare ultrasonic sensor Trigger pin as Output
+
+  pinMode(enA, OUTPUT); // declare as output for L298 Pin enA
+  pinMode(in1, OUTPUT); // declare as output for L298 Pin in1
+  pinMode(in2, OUTPUT); // declare as output for L298 Pin in2
+  pinMode(in3, OUTPUT); // declare as output for L298 Pin in3
+  pinMode(in4, OUTPUT); // declare as output for L298 Pin in4
+  pinMode(enB, OUTPUT); // declare as output for L298 Pin enB
+
+  analogWrite(enA, 200); // Write The Duty Cycle 0 to 255 Enable Pin A for Motor1 Speed
+  analogWrite(enB, 200); // Write The Duty Cycle 0 to 255 Enable Pin B for Motor2 Speed
+
+  pinMode(servo, OUTPUT);
+
+  for (int angle = 70; angle <= 140; angle += 5)
+  {
+    servoPulse(servo, angle);
+  }
+  for (int angle = 140; angle >= 0; angle -= 5)
+  {
+    servoPulse(servo, angle);
+  }
+
+  for (int angle = 0; angle <= 70; angle += 5)
+  {
+    servoPulse(servo, angle);
+  }
+
+  distance_F = Ultrasonic_read();
+
+  delay(500);
 }
 
 void loop()
 {
-    // establish variables for duration of the ping, and the distance result
-    // in inches and centimeters:
+  //==============================================
+  //     Line Follower and Obstacle Avoiding
+  //==============================================
 
-    // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-    pinMode(pingPin, OUTPUT);
-    digitalWrite(pingPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(pingPin, HIGH);
-    delayMicroseconds(5);
-    digitalWrite(pingPin, LOW);
+  distance_F = Ultrasonic_read();
+  Serial.print("D F=");
+  Serial.println(distance_F);
 
-    // The same pin is used to read the signal from the PING))): a HIGH pulse
-    // whose duration is the time (in microseconds) from the sending of the ping
-    // to the reception of its echo off of an object.
-    pinMode(pingPin, INPUT);
-    duration = pulseIn(pingPin, HIGH);
+  // if Right Sensor and Left Sensor are at White color then it will call forword function
+  if ((digitalRead(R_S) == 0) && (digitalRead(L_S) == 0))
+  {
+    if (distance_F > Set)
+    {
+      forword();
+    }
+    else
+    {
+      Check_side();
+    }
+  }
 
-    // convert the time into a distance
-    inches = microsecondsToInches(duration);
-    cm = microsecondsToCentimeters(duration);
+  // if Right Sensor is Black and Left Sensor is White then it will call turn Right function
+  else if ((digitalRead(R_S) == 1) && (digitalRead(L_S) == 0))
+  {
+    turnRight();
+  }
 
-    delay(100);
+  // if Right Sensor is White and Left Sensor is Black then it will call turn Left function
+  else if ((digitalRead(R_S) == 0) && (digitalRead(L_S) == 1))
+  {
+    turnLeft();
+  }
+
+  delay(10);
 }
 
-long microsecondsToInches(long microseconds)
+void servoPulse(int pin, int angle)
 {
-    return microseconds / 74 / 2;
+  int pwm = (angle * 11) + 500; // Convert angle to microseconds
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(pwm);
+  digitalWrite(pin, LOW);
+  delay(50); // Refresh cycle of servo
 }
 
-long microsecondsToCentimeters(long microseconds)
+//**********************Ultrasonic_read****************************
+long Ultrasonic_read()
 {
-    return microseconds / 29 / 2;
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  long time = pulseIn(echo, HIGH);
+  return time / 29 / 2;
 }
 
+void compareDistance()
+{
+  if (distance_L > distance_R)
+  {
+    turnLeft();
+    delay(500);
+    forword();
+    delay(600);
+    turnRight();
+    delay(500);
+    forword();
+    delay(600);
+    turnRight();
+    delay(400);
+  }
+  else
+  {
+    turnRight();
+    delay(500);
+    forword();
+    delay(600);
+    turnLeft();
+    delay(500);
+    forword();
+    delay(600);
+    turnLeft();
+    delay(400);
+  }
+}
 
-// the question is wether to take inches or seconds ? 
+void Check_side()
+{
+  Stop();
+  delay(100);
+  for (int angle = 70; angle <= 140; angle += 5)
+  {
+    servoPulse(servo, angle);
+  }
+  delay(300);
+  distance_R = Ultrasonic_read();
+  Serial.print("D R=");
+  Serial.println(distance_R);
+  delay(100);
+  for (int angle = 140; angle >= 0; angle -= 5)
+  {
+    servoPulse(servo, angle);
+  }
+  delay(500);
+  distance_L = Ultrasonic_read();
+  Serial.print("D L=");
+  Serial.println(distance_L);
+  delay(100);
+  for (int angle = 0; angle <= 70; angle += 5)
+  {
+    servoPulse(servo, angle);
+  }
+  delay(300);
+  compareDistance();
+}
 
+void forword()
+{                          // forword
+  digitalWrite(in1, LOW);  // Left Motor backword Pin
+  digitalWrite(in2, HIGH); // Left Motor forword Pin
+  digitalWrite(in3, HIGH); // Right Motor forword Pin
+  digitalWrite(in4, LOW);  // Right Motor backword Pin
+}
+
+void backword()
+{                          // backword
+  digitalWrite(in1, HIGH); // Left Motor backword Pin
+  digitalWrite(in2, LOW);  // Left Motor forword Pin
+  digitalWrite(in3, LOW);  // Right Motor forword Pin
+  digitalWrite(in4, HIGH); // Right Motor backword Pin
+}
+
+void turnRight()
+{                          // turnRight
+  digitalWrite(in1, LOW);  // Left Motor backword Pin
+  digitalWrite(in2, HIGH); // Left Motor forword Pin
+  digitalWrite(in3, LOW);  // Right Motor forword Pin
+  digitalWrite(in4, HIGH); // Right Motor backword Pin
+}
+
+void turnLeft()
+{                          // turnLeft
+  digitalWrite(in1, HIGH); // Left Motor backword Pin
+  digitalWrite(in2, LOW);  // Left Motor forword Pin
+  digitalWrite(in3, HIGH); // Right Motor forword Pin
+  digitalWrite(in4, LOW);  // Right Motor backword Pin
+}
+
+void Stop()
+{                         // stop
+  digitalWrite(in1, LOW); // Left Motor backword Pin
+  digitalWrite(in2, LOW); // Left Motor forword Pin
+  digitalWrite(in3, LOW); // Right Motor forword Pin
+  digitalWrite(in4, LOW); // Right Motor backword Pin
+}
